6f4fa4568e457ae4ebc455af817bbdb4401abc35
Francisco Gomez <pgomezdev@gmail.com>
2023-02-28 19:00:05 -0500

    Deploy n4

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "https://PaconcioDev.github.io/rick-and-morty-API/",
   plugins: [react()],
-  base: "/rick-and-morty-API/",
 })
